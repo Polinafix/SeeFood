0995f865f07edb20ff900db9838da2060596cac6
polinafiksson <pd.fiksson@gmail.com>
2017-12-12 15:15:20 -0600

    camera functionality

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import Vision
+import CoreML
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    //new object
+    let imagePicker = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    //after the image was picked
+    //picker = imagePicker
+    //inside the info parameter is contained the image that the user picked
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //1.check that the image was indeed picked - use the key for the original image
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            //set the image view to the image that was picked
+            imageView.image = userPickedImage
+        }
+        
+        //dismiss imagePicker
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
 
 }
 
